b7f45b738f64cb096de1d2a28abf59bb8c98fe7e
Michael Radvak <michael.radvak@codeandtheory.com>
2013-03-26 17:26:10 -0400

    Cleaning up some code.

--- public/scripts/app.tea.js
+++ public/scripts/app.tea.js
@@ -4,55 +4,27 @@ require(["jquery", pv + "dropdown.js", pv + "prettify.js", pl + 'Noduino.js', pl
   var Noduino    = null;
   var button_pin = 6;
   var led_pin    = 12;
+  var led        = null;
 
-  var readyLED = function(led) {
-    walkLED.listLED[walkLED.sorting.indexOf(led.pin)] = led;
-    if (walkLED.listLED.length == walkLED.maxLEDs) {
-      Noduino.log('success', 'Loaded all LEDs');
-      startSequence(-1, walkLED.interval);
-    }
+  var readyLED = function(LED) {
+    led = LED;
   };
 
   function addButton(Button, dir) {
     Button.on('release', function(e) {
       $('#btn').removeClass('btn-warning');
+      led.setOff();
     });
 
     Button.on('push', function(e) {
       Noduino.log('gui', 'Pushed Button ' + e.pin);
       $('#btn').addClass('btn-warning');
+      led.setOn();
     });
   }
 
-  function stepper() {
-    var next = walkLED.current + walkLED.direction;
-    if (next == walkLED.maxLEDs + 1) {
-      next = 1; }
-    if (next == 0) {
-      next = walkLED.maxLEDs; }
-    walkLED.current = next;
-    $('#leds .btn').removeClass('btn-warning');
-    for (var i = 1; i <= walkLED.maxLEDs; i++) {
-      walkLED.listLED[i-1].setOff();
-    }
-    walkLED.listLED[(walkLED.current-1)].setOn();
-    Noduino.log('gui', 'setting on LED #led-' + walkLED.current);
-    $('#led-' + walkLED.current).addClass('btn-warning');
-  }
-
-  function startSequence(step, interval) {
-    if (walkLED.listLED.length != walkLED.maxLEDs) {
-      return;
-    };
-    clearInterval(walkLED.currentStepper);
-    walkLED.direction = step || 1;
-    walkLED.currentStepper = setInterval(function() {
-      return stepper();
-    }, interval);
-  }
-
   var createObjects = function(board) {
-    // board.withLED({pin: led}, function(err, LED) { readyLED(LED); });
+    board.withLED({pin: led}, function(err, LED) { readyLED(LED); });
     board.withButton({pin:  button_pin}, function(err, Button) { addButton(Button); $('#btn').click(function(e) {e.preventDefault(); Button.setOn(); Button.setOff(); }); });
   };
 
@@ -61,7 +33,7 @@ require(["jquery", pv + "dropdown.js", pv + "prettify.js", pl + 'Noduino.js', pl
       e.preventDefault();
 
       if (!Noduino || !Noduino.connected) {
-        Noduino = new NoduinoObj({debug: true, host: 'http://teapi.local:8090', logger: {container: '#connection-log'}}, Connector, Logger);
+        Noduino = new NoduinoObj({debug: false, host: 'http://teapi.local:8090', logger: {container: '#connection-log'}}, Connector, Logger);
         Noduino.connect(function(err, board) {
           $('#connection-status .alert').addClass('hide');
           if (err) {
